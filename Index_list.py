b55bde927e8244a4f540cf4eff0bbafe23da8eb2
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-08 15:05:39 +0530

    Create Index_list.py

---
+++ Index_list.py
@@ -0,0 +1,9 @@
+color_list = ["Red", "Orange", "Yellow", "Pink", "Green", "Blue", "Purple", "Black", "White"]
+# Write your code here
+N=int(input())
+print(color_list[N])
+
+"""
+input:4
+output:Green
+"""
