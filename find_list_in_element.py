9ec7eb80120ca5d32e7d53c0fa77840117b04b45
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-08 15:14:53 +0530

    Create find_list_in_element.py

---
+++ find_list_in_element.py
@@ -0,0 +1,12 @@
+L = ["5", "eat", "9.80", "Water", "python", "-678", "7685.26", "-2.5", "sing"]
+
+# Write your code here
+string=input()
+if string in L:
+    print("True")
+else:
+    print("False")
+"""
+input:water
+output:True
+"""
