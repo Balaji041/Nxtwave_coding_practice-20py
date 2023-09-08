06e6222a16d05cbbffbb263a329c7fc1f533c6d4
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-08 15:08:40 +0530

    Create list_indexing-2.py

---
+++ list_indexing-2.py
@@ -0,0 +1,15 @@
+programming_languages_list = ["Python", "Java", "Ruby", "C", "C++", "Go", "R", "JavaScript", "Swift", "PHP", "Kotlin", "Perl"]
+# Write your code here
+N=int(input())
+for i in range(N):
+    num=int(input())
+    print(programming_languages_list[num])
+
+"""
+input:2
+1
+4
+output:
+Java
+C++
+"""
