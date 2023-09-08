d5b653b6d6edd30f4743809aa6ec47d1d164993f
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-08 15:31:42 +0530

    Create list_concatenation-2.py

---
+++ list_concatenation-2.py
@@ -0,0 +1,16 @@
+L = ["apple", "78", "970.03"]
+
+# Write your code here
+N=int(input())
+for i in range(N):
+    num=input()
+    L=L+[num]
+print(L)
+
+"""
+input:2
+38
+grapes
+output:['apple', '78', '970.03', '38', 'grapes']
+
+"""
